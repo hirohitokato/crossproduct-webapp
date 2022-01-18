33d35126a69b462f3c49857227c4adf9941562c8
Kato Hirohito <Kato.Hirohito@exc.epson.co.jp>
2022-01-19 08:29:11 +0900

    add 3d graph

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import numpy as np
+from matplotlib import pyplot as plt
+import matplotlib.cm as cm
 
 def main():
 
@@ -40,11 +42,29 @@ def main():
                         min_value=-10,
                         max_value=10,
                         value=0)
-                        
+
+    # 計算                        
     va = np.array([a_x, a_y, a_z])
     vb = np.array([b_x, b_y, b_z])
     cross = np.cross(va, vb)
+
+    # グラフのプロット
     st.write(f'cross product: {cross}')
+    fig = plt.figure()
+    ax = fig.gca(projection='3d')
+    ax.set_xlim([-10,10])
+    ax.set_ylim([-10,10])
+    ax.set_zlim([-10,10])
+    ax.set(xlabel='x', ylabel='y', zlabel='z')
+    origin = np.zeros((3,3))
+    vectors = np.array((va,vb,cross))
+    cmap = cm.get_cmap(name='rainbow')
+
+    ax.quiver(origin[:,0],origin[:,1],origin[:,2],
+              vectors[:,0],vectors[:,1],vectors[:,2],
+              colors=['blue', 'green', 'red'])
+    st.pyplot(fig)
+
 
 if __name__ == '__main__':
     main()
